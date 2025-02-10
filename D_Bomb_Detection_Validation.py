805db73f5bdb35c26ff6bebfe457efe292e62fc4
JiruGutema <jethior1@gmail.com>
2025-02-10 11:08:34 +0300

    bomb detection

--- D_Bomb_Detection_Validation.py
+++ D_Bomb_Detection_Validation.py
@@ -1,40 +1,43 @@
-
-n,m = map(int,input().split())
-mat = [list(map(str, list(input()))) for _ in range(n)]
-directions = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]
-def inbound(x,y):
-    if 0 <= x < n-1 and 0 <= y < m-1:
-        return True
-    return False
-res = []
-nums = set(str(i) for i in range(1, 9))
-return_flag = True
+n, m = map(int, input().split())
+flag = True
+grid = []
 for i in range(n):
-    for j in range(m):
-        # check for the invalid in the matrix
-        flag = True
-        # current element for the comparison
-        
-        
-        for dx,dy in directions:
-            t ,s = i + dx ,j + dy
-            if inbound(t,s):
-                current = mat[i][j]
-                neighbour = mat[t][s]
-                
-                # have to skip the current element
-                if current != neighbour:
-                    if current in nums and neighbour != "*":
-                        flag = False
-                        break
-                    elif current == "." and neighbour == "*":
-                        flag = False
-                        break
+    row = input()
+    grid.append(row)
 
-        if not flag:
-            return_flag = flag
-            break
-if return_flag:
+for row in range(n):
+    for col in range(m):
+        if grid[row][col] == "*":
+            continue
+        count = 0
+
+        if 0 <= col-1 < m and grid[row][col-1] == "*":
+            count += 1
+        if 0 <= col+1 < m and grid[row][col+1] == "*":
+            count += 1
+
+        if 0 <= row-1 < n and grid[row-1][col] == "*":
+            count += 1
+        if 0 <= row+1 < n and grid[row+1][col] == "*":
+            count += 1
+            
+        if 0 <= col-1 < m and 0 <= row+1 < n and grid[row+1][col-1] == "*":
+            count += 1
+        if 0 <= col-1 < m and 0 <= row-1 < n and grid[row-1][col-1] == "*":
+            count += 1
+        
+        if 0 <= col+1 < m and 0 <= row+1 < n and grid[row+1][col+1] == "*":
+            count += 1
+        if 0 <= col+1 < m and 0 <= row-1 < n and grid[row-1][col+1] == "*":
+            count += 1
+
+        if count != 0 and  grid[row][col] == ".":
+            flag = False
+        elif grid[row][col] != "." and int(grid[row][col]) != count:
+            flag = False
+
+if flag:
     print("YES")
 else:
     print("NO")
+
